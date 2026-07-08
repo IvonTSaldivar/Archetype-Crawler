03455f32dc693db0bbd384b13770c9e07e2a6236
Ivon <newtoa663@gmail.com>
2026-07-08 12:03:22 -0600

    Bard data now includes suggestion and mass suggestion

---
+++ updateTemplates/createArchetypePairCache.js
@@ -0,0 +1,9 @@
+// archetypepairCache.js format:
+/*
+    See archetypepairCacheTemplate.json for the structure of the archetype pair cache.
+*/
+
+
+// For each object in dataCache["keys-0"], do the following:
+    // For each object in [hash-1][keys-1], compare it's set-2 to every other set-2 in the same [hash-1][keys-1] object.
+    // If any of them match, add the key-1 of the matching object to the set-2 of the pairCache.
